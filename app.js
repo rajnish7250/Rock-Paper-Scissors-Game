1b948bbe6307c66b83c565daab9a012176265330
Rajnish Kumar <106313846+rajnish7250@users.noreply.github.com>
2024-05-30 19:43:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,180 +1,126 @@
-let userScore=0;
-let compScore=0;
-const choices=document.querySelectorAll(".choice");
-let msg=document.querySelector("#msg");
+var choices=document.querySelectorAll(".choice")
+var msg=document.querySelector(".msg");
+var userScoreJ=0;
+var compScoreJ=0;
+var userscore=document.querySelector("#userscore");
+var compscore=document.querySelector("#compscore");
+var table=document.querySelector("table");
+var userchoicee;
+var compchoicee;
+var statuss;
+var sno=0;
 
-const genCompChoice = () => {
-    let options=["rock","paper","scissors"]
-    const randIdx=Math.floor(Math.random()*3);
-    return options[randIdx];
-}
-
-const showWinner =(userWin) => {
-    if(userWin){
-        console.log("you win");
-        userScore++;
-        document.querySelector("#user-score").innerText=userScore;
-        msg.innerText="You Win!!";
-        msg.style.backgroundColor="green";
-    }
-    else{
-        console.log("Computer win");
-        compScore++;
-        document.querySelector("#comp-score").innerText=compScore;
-        msg.innerText="Computer Win!!";
-        msg.style.backgroundColor="red";
-    }
-}
-
-const gameDraw = () =>
-{
-    console.log("Game is Draw:");
-    msg.innerText="Game is Draw";
-    msg.style.backgroundColor="black";
-}
-
-const playGame= (userChoice) => 
-{
-    console.log("User-Choice ",userChoice);
-    const compChoice=genCompChoice();
-    console.log("Comp-Choice ",compChoice);
-    let userWin=true;
-    if(userChoice===compChoice){
-        // Draw Game
-        gameDraw();
-    }
-    else if(userChoice==="paper")
-    {
-        // scissors, rock
-        userWin= compChoice==="scissors"?false: true;
-        showWinner(userWin);
-    }
-    else if(userChoice==="scissors")
-    {
-        //rock,paper
-        userWin = compChoice==="rock"?false: true;
-        showWinner(userWin);
-    }
-    else{
-        //paper,scissors
-        userWin=compChoice==="paper"?false:true;
-        showWinner(userWin);
-    }
-
-    
-
-
-
-
-}
-
-choices.forEach((choice) => {
-    console.log(choice);
-    choice.addEventListener("click", () =>{
-        const userChoice=choice.getAttribute("id");
-        playGame(userChoice);
+choices.forEach((choice) =>{
+    choice.addEventListener("click",() =>{
+        var userchoice=choice.getAttribute("id");
+        userchoicee=userchoice;
+        
+        playGame(userchoice);
     })
+})
+
+var genCompChoice = () =>{
+    var options=["rock","paper","scissors"];
+    var idx=Math.floor(Math.random()*3);
+    return options[idx];
+}
+
+var playGame = (userchoice) => {
+    console.log(`UserChoice: ${userchoice}`);
+    var compchoice=genCompChoice();
+    compchoicee=compchoice;
+    console.log(`Computer Choice: ${compchoice}`);
+    var userWin=true;
+
+    if(userchoice===compchoice)
+        {
+            var status="Match was drawn";
+            console.log(status);
+            updateMsgBoard(status);
+        }
+    else if(userchoice==="rock")
+        {
+            //paper, scissors
+            userWin=compchoice==="paper"?false:true;
+            showWinner(userWin);
+        }
+    else if(userchoice==="paper")
+        {
+            //rock,scissors
+            userWin=compchoice==="rock"?true:false;
+            showWinner(userWin);
+        }
+    else if(userchoice==="scissors")
+        {
+            //paper, rock
+            userWin=compchoice==="paper"?true:false;
+            showWinner(userWin);
+        }
+
+}
+
+var showWinner = (userWin) =>
+    {
+        if (userWin)
+            {
+                var status="User Win";
+                console.log(status);
+                updateMsgBoard(status);
+            }
+        else{
+            var status="Comp win";
+            console.log(status);
+            updateMsgBoard(status);
+        }
     }
-)
 
+var updateMsgBoard = (status) =>{
+    if(status=="User Win")
+        {
+            msg.style.backgroundColor="green";
+            msg.innerText=status;
+            userScoreJ++;
+            userscore.innerText=userScoreJ;
+            updateTable(status);
+        }
+    else if(status==="Comp win")
+        {
+            msg.style.backgroundColor="red";
+            msg.innerText=status;
+            compScoreJ++;
+            compscore.innerText=compScoreJ;
+            updateTable(status);
+        }
+    else
+    {
+        msg.style.backgroundColor="black";
+        msg.innerText=status;
+        updateTable(status);
 
+    }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let options=["rock","paper","scissors"];
-// let optionSelected=Math.floor(Math.random(options)*3);
-// let msg=document.querySelector("#msg");
-// let user=document.querySelector("#user-score");
-// let comp=document.querySelector("#comp-score");
-// let userScore=0;
-// let compScore=0;
-
-// let rock=document.querySelector("#rock");
-// rock.onclick= () => {
-//     let optionSelected=options[Math.floor(Math.random(options)*3)];
-//     if(optionSelected==="paper"){
-//         compScore++;
-//         comp.innerText=compScore;
-//         msg.innerText=`Computer win.  Computer_Choice:${optionSelected}, User_Choice:Rock`;
-//         msg.style.backgroundColor="red";
-//     }
-//     else if (optionSelected==="rock"){
-//         msg.innerText=`Match Draw.  Computer_Choice:${optionSelected}, User_Choice:Rock`;
-//     }
-//     else{
-//         userScore++;
-//         user.innerText=userScore;
-//         msg.innerText=`You win.  Computer_Choice:${optionSelected}, User_Choice:Rock`;
-//         msg.style.backgroundColor="green";
-//     }
-//     // alert(`User Selected Rock and Computer Selected ${options[optionSelected]}`);
-    
-// }
-
-
-
-
-// let paper=document.querySelector("#paper");
-// paper.onclick=() =>{
-//     let optionSelected=options[Math.floor(Math.random(options)*3)];
-//     if(optionSelected==="paper"){
-//         compScore++;
-//         comp.innerText=compScore;
-//         msg.innerText=`Computer win.  Computer_Choice:${optionSelected}, User_Choice:Rock`;
-//         msg.style.backgroundColor="red";
-//     }
-//     else if (optionSelected==="rock"){
-//         msg.innerText=`Match Draw.  Computer_Choice:${optionSelected}, User_Choice:Rock`;
-//     }
-//     else{
-//         userScore++;
-//         user.innerText=userScore;
-//         msg.innerText=`You win.  Computer_Choice:${optionSelected}, User_Choice:Rock`;
-//         msg.style.backgroundColor="green";
-//     }
-//     // alert(`User Selected Paper and Computer Selected ${options[optionSelected]}`);
-// }
-
-
-
-
-
-// let scissors=document.querySelector("#scissors");
-// scissors.onclick = () =>{
-//     let optionSelected=options[Math.floor(Math.random(options)*3)];
-//     if(optionSelected==="paper"){
-//         compScore++;
-//         comp.innerText=compScore;
-//         msg.innerText=`Computer win.  Computer_Choice:${optionSelected}, User_Choice:Rock`;
-//         msg.style.backgroundColor="red";
-//     }
-//     else if (optionSelected==="rock"){
-//         msg.innerText=`Match Draw.  Computer_Choice:${optionSelected}, User_Choice:Rock`;
-//     }
-//     else{
-//         userScore++;
-//         user.innerText=userScore;
-//         msg.innerText=`You win.  Computer_Choice:${optionSelected}, User_Choice:Rock`;
-//         msg.style.backgroundColor="green";
-//     }
-//     // alert(`User Selected scissors and Computer Selected ${options[optionSelected]}`);
-// }
+var updateTable = (status) =>{
+    var row=table.insertRow(1);
+    var cell1=row.insertCell(0);
+    var cell2=row.insertCell(1);
+    var cell3=row.insertCell(2);
+    var cell4=row.insertCell(3);
+    sno++;
+    cell1.innerHTML=sno;
+    cell2.innerHTML=userchoicee;
+    cell3.innerHTML=compchoicee;
+    cell4.innerHTML=status;
+    if(status==="User Win")
+        {
+            cell4.style.backgroundColor="green";
+        }
+    else if(status==="Comp win")
+        {
+            cell4.style.backgroundColor="red";
+        }
+    else{
+        cell4.style.backgroundColor="black";
+        cell4.style.color="white";
+    }
+}
